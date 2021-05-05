7cfd676adedca30319e5d1d8a616635681c56f9e
tshjones <tshjones@yahoo.com>
2021-05-04 19:08:27 -0500

    first commit, map class started

---
+++ Rogue/Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rogue
+{
+    public class Map
+    {
+        public int Level { get; set; }
+        public List<List<char>> Chart { get; set; }
+
+        public Map (int Level, int x = 20, int y = 20)
+        {
+
+        }
+    }
+}
---
+++ Rogue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
